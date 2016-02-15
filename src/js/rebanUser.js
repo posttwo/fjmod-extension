0f90592d8513d10169a178fd2d2febfc5d3656e0
Mateusz Ulinski <post159510@gmail.com>
2016-02-15 15:17:55 +0000

    Show reban button

--- src/js/rebanUser.js
+++ src/js/rebanUser.js
@@ -1,7 +1,7 @@
 chrome.storage.sync.get({
     showreban: false,
   }, function(items) {
-	if( items.showreban && $('#ban_status').is(':visible') )
+	if( items.showreban && !$('#ban_user').is(':visible') )
 	{
 		$('.contentTitle').first().append('<input type="button" id="ban_user" style="display:inline;" value="Ban this user" class="banuser" onclick="banUser(contentId);">');
 		console.log('test');
