258b8ed6a92c41e684678f20c194cb1f3b7808fa
Posttwo <post159510@gmail.com>
2016-11-04 15:42:43 +0000

    add ban button to ban requests

--- src/js/banRequestsTicker.js
+++ src/js/banRequestsTicker.js
@@ -37,7 +37,7 @@ function getBanRequests()
 					table += '<td><a href="/user/'+ this.user_name +'">' + this.user_name + '</a></td>';
 					table += '<td>' + this.reason + '</td>';
 					table += '<td><a href="/user/'+ this.requester_name +'">' + this.requester_name + '</a></td>';
-					table += '<td><button class="DENY" data-requestid="'+ this.id +'">DENY</button><button class="OK" data-requestid="'+ this.id +'">OK</button></td>';
+					table += '<td><button class="DENY" data-requestid="'+ this.id +'">DENY</button><button class="OK" data-requestid="'+ this.id +'">OK</button><button class="BAN" data-requestid="'+ this.id +'" data-userid="' + this.user_id + '">Ban</button></td>';
 					table += '</tr>';
 				});
                 $('<div><table border="1" width="100%" align="center" cellspacing="0">' + table + '</table></div>').dialog({
@@ -63,6 +63,11 @@ function getBanRequests()
 				var id = $(this).attr("data-requestid");
 				handle(id, 'PERMITTED');
 			})
+		 $('.BAN').click(function(){
+				var id = $(this).attr("data-requestid");
+				handle(id, 'PERMITTED');
+				banUser($(this).attr("data-userid"));
+			}) 
 		}
 	});
 }
