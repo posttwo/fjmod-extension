1e4768ec857b6e8c78e5694f0fa7696bf20caf69
Hunman <sanyi.exe@gmail.com>
2017-03-05 17:18:50 +0100

    Added the confirmation dialog (#29)
    * Clear cookies moving
    
    1) Instead of inserting "Mod setting" after "Clear cookies", I replace it with it
    2) I put a clear cookies button inside the Mod settings window (side effect: non-mods can press it too now)
    3) Fixed tabulation inside inject.js
    
    * Rainbow text removal/masking option
    
    Added option (available to non-mods) that inserts a small CSS that removes/overrides rainbow text
    
    * Flag all comments confirmation
    
    Added a confirmation dialog that lists the comments once again

--- src/js/flagAllUserComments.js
+++ src/js/flagAllUserComments.js
@@ -1,24 +1,66 @@
 posttwo.ddd("flagAllComments.js has been loaded");
-$('.contentTitle:first').append('<div class="sbtBzttn"><input type="button" class="modBtn" id="PT_Flag_All_Button" style="display:inline;" value="Flag Comments Page"></div>');
+
+var place = $('#userCommentsList > .title > h2');
+if (!place.length)
+	place = $('.contentTitle:first');
+
+place.append('&nbsp;<div class="sbtBzttn" style="display: inline-block;"><input type="button" class="modBtn" id="PT_Flag_All_Button" style="display:inline;" value="Mass flag comments"></div>');
+
+
 //Get all comments visible
 $('#PT_Flag_All_Button').click(function(){
+	posttwo.ddd('Mass flag button pressed');
+	var menuDialog = $.extend($('<div id="PT_MassFlagForm">'), {
+		addComment: function (url, commentID, text) {
+            menuDialog.append('<div class="comm">' + text + '<br/><small>@<a href="' + url + '">'+ url +'</a></small></div>');
+        },
+    });
+
 	var comments = $.map($(".cl_tx a"), function(li) {
 		return $(li).attr("href");
 	});
 	var commentIDs = $.map($(".cl_tx a"), function(li) {
 		return $(li).find('span').text();
 	});
-	$.each(comments, function(index, value) {
-		posttwo.ddd("Getting :" + value);
-		$.ajax({
-			url:"https://funnyjunk.com" + value,
-			type:'GET',
-			success: function(data){
-			   var x = $(data).find('a' + commentIDs[index]).parent().find('.ctBox3').attr('data-aid')
-			   posttwo.ddd("Flagging: "+x);
-			   posttwo.flagCommentSpam(x);
-			}
-		});
+	var texts = $.map($(".cl_tx a"), function(li) {
+		return $(li).find('span').parent().clone().children().remove().end().text().substring(3);
 	});
+
+
+	$.each(comments, function(index, value){
+		menuDialog.addComment(comments[index], commentIDs[index], texts[index]);
+	});
+
+    menuDialog.dialog({
+        title: "Are you sure you want to flag the following comments?",
+        closeOnEscape: true,
+        resizable: false,
+        width: 600,
+        minHeight: 450,
+        buttons: {
+            "Maybe not": function () {
+                $(this).dialog("close");
+            },
+            "Purge 'em": function () {
+                posttwo.ddd('Mass flagging comments begins');
+				$.each(comments, function(index, value) {
+					posttwo.ddd("\tGetting: " + value);
+					$.ajax({
+						url:"https://funnyjunk.com" + value,
+						type:'GET',
+						success: function(data){
+						   var x = $(data).find('a' + commentIDs[index]).parent().find('.ctBox3').attr('data-aid')
+						   posttwo.ddd("\tFlagging: " + x);
+						   posttwo.flagCommentSpam(x);
+						}
+					});
+				});
+				posttwo.ddd('Mass flagging comments ended');
+				$(this).dialog("close");
+            },
+        }
+    }).parent().attr("id", "setboxthing");
+    $("div.ui-draggable").css("overflow", "visible")
 });
+
 //$('a#89').parent().find('.ctBox3').attr('data-aid')
