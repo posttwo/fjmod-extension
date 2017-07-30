8936d363faa338154d52136d36f6837fafe09a9d
Posttwo <post159510@gmail.com>
2017-07-30 14:56:48 +0100

    Fix

--- src/js/sfwRatingsAlert.js
+++ src/js/sfwRatingsAlert.js
@@ -1,18 +1,15 @@
 posttwo.ddd("sfwRatingsAlert.js has been loaded");
-/*
-Code fixed up by a person that doesn't want to be named because they're in the furry porn degeneracy discord server 
-*/
 
-var posttwo = new function(){};
 posttwo.sfwRatingsAlert = function(){};
 posttwo.sfwRatingsAlert.element = document.querySelectorAll(".flexModAlerts2[href='/sfw_mod/contents/']");
+
 for(var i = 0, l = posttwo.sfwRatingsAlert.element.length; i < l; i++) {
-var e = posttwo.sfwRatingsAlert.element[i];
-var unrated = (e ? parseInt(e.innerHTML) : 0);
-if (unrated >= 50){
-     $( "<img src='http://i.heykidwannayiff.com/aef79e.gif'>" ).insertBefore( ".flexModAlerts2:first" );
-     $( "<img src='http://i.heykidwannayiff.com/aef79e.gif'><br><h1 style='color:red;font-size:50px;'>!!DO SFW RATINGS!!</h1>" ).insertAfter( ".flexModAlerts2:last" );
-     $(".flexModAlerts2").css({"font-size": "25px", "background-image": "-webkit-linear-gradient(top,#ff4b4b,#981414)"});
-    }
+  var e = posttwo.sfwRatingsAlert.element[i];
+  var unrated = (e ? parseInt(e.innerHTML) : 0);
+  if (unrated >= 50){
+      $( "<img src='http://i.heykidwannayiff.com/aef79e.gif'>" ).insertBefore( ".flexModAlerts2:first" );
+      $( "<img src='http://i.heykidwannayiff.com/aef79e.gif'><br><h1 style='color:red;font-size:50px;'>!!DO SFW RATINGS!!</h1>" ).insertAfter( ".flexModAlerts2:last" );
+      $(".flexModAlerts2").css({"font-size": "25px", "background-image": "-webkit-linear-gradient(top,#ff4b4b,#981414)"});
   }
+}
 
