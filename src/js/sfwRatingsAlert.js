510d98793766824657f70faa916b693693a8d872
thunderysteak <thunderysteak@live.co.uk>
2017-07-30 03:35:51 +0200

    Total code rework because shit was broken
    What the fuck is even javascript

--- src/js/sfwRatingsAlert.js
+++ src/js/sfwRatingsAlert.js
@@ -1,11 +1,18 @@
 posttwo.ddd("sfwRatingsAlert.js has been loaded");
-posttwo.sfwRatingAlert = function(){};
-//I wanted to do this myself but I almost got stabbed by a racoon for shitty code
-posttwo.sfwRatingsAlert.element = $(".flexModAlerts2[href='/sfw_mod/contents/']");
-posttwo.sfwRatingsAlert.unrated = (element ? parseInt(element.text()) : 0);
 
+/*
+Code fixed up by a person that doesn't want to be named because they're in the furry porn degeneracy discord server 
+*/
+
+var posttwo = new function(){};
+posttwo.sfwRatingsAlert = function(){};
+posttwo.sfwRatingsAlert.element = document.querySelectorAll(".flexModAlerts2[href='/sfw_mod/contents/']");
+for(var i = 0, l = posttwo.sfwRatingsAlert.element.length; i < l; i++) {
+var e = posttwo.sfwRatingsAlert.element[i];
+var unrated = (e ? parseInt(e.innerHTML) : 0);
 if (unrated >= 50){
-    $( "<img src='http://i.heykidwannayiff.com/aef79e.gif'>" ).insertBefore( ".flexModAlerts2:first" );
-    $( "<img src='http://i.heykidwannayiff.com/aef79e.gif'><br><h1 style='color:red;font-size:50px;'>!!DO SFW RATINGS!!</h1>" ).insertAfter( ".flexModAlerts2:last" );
-    $(".flexModAlerts2").css({"font-size": "25px", "background-image": "-webkit-linear-gradient(top,#ff4b4b,#981414)"});
+     $( "<img src='http://i.heykidwannayiff.com/aef79e.gif'>" ).insertBefore( ".flexModAlerts2:first" );
+     $( "<img src='http://i.heykidwannayiff.com/aef79e.gif'><br><h1 style='color:red;font-size:50px;'>!!DO SFW RATINGS!!</h1>" ).insertAfter( ".flexModAlerts2:last" );
+     $(".flexModAlerts2").css({"font-size": "25px", "background-image": "-webkit-linear-gradient(top,#ff4b4b,#981414)"});
     }
+  }
